a472841ccd74296762f9793b3387f38ddf2a662b
PedroPerez14 <756642@unizar.es>
2021-12-15 10:05:13 +0100

    mas saliencia yujuuuu
    creo que tengo el pie roto y lo llevo muy abotijado pero who cares

--- Assets/Scripts/SaliencyMapsGenerator.cs
+++ Assets/Scripts/SaliencyMapsGenerator.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class SaliencyMapsGenerator : MonoBehaviour
 {
+    [Header("")]
+    public TextAsset videoList;                             //This should be a .txt file generated at the same time as the logs that we are going to process
+    public TextAsset[] logsToProcess;                       //We'll validate that the video clip we're referring to from the log's name 
+    public float t;                                         //Instant of the 
+
+    //---------- We'll let the users choose which maps they want to output --------//
+    public bool outputFrame = true;
+    public bool outputFixationMap = true;
+    public bool outputHeatMap = true;
+    public bool outputSaliencyMap = true;
+    //-----------------------------------------------------------------------------//
+
+
     // Start is called before the first frame update
     void Start()
     {
